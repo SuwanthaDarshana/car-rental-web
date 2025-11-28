24da2a13945930b8ffe3b30414059141d61c930f
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-11-29 01:09:13 +0530

    add file

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import { navbarStyles as styles } from "../assets/dummyStyles";
 import { Link } from "react-router-dom";
 import logo from "../assets/logocar.png";
@@ -6,12 +6,20 @@ import logo from "../assets/logocar.png";
 function Navbar() {
   const [scrolled, setScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(()=> localStorage.getItem('authToken'));
+  const location = useLocation();
+  const navigate = useNavigate();
 
   const navLinks = [
     { to: "/", label: "Home" },
     { to: "/cars", label: "Cars" },
     { to: "/contact", label: "Contact" },
   ];
+
+  const isActive = (path) => {
+    if(path=='/') return location.pathname === '/';
+    return location.pathname.startsWith(path);
+  }
 
   return (
     <nav
@@ -34,9 +42,25 @@ function Navbar() {
                 <img src={logo} alt="logo" className="h-[1em] w-auto block" 
                     style={{display: 'block', objectFit: 'contain'}}
                 />
+                <span className={styles.logoText}>CarBook</span>
             </div>
+            
 
             </Link>
+            <div className={styles.navLinksContainer}>
+              <div className={styles.navLinksInner}>
+                {navLinks.map((link,index) => (
+                  <React.Fragment key={link.to}>
+                  <Link to={link.to} className={`${styles.navLink.base}`}> ${
+                    isA
+                  }
+
+
+                  </Link>
+                  </React.Fragment>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
