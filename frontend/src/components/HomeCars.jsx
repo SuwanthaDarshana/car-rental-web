9ed56a737455d2798aed3f04676955d1d0186dea
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-11-30 23:36:34 +0530

    add lucide icon

--- frontend/src/components/HomeCars.jsx
+++ frontend/src/components/HomeCars.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import { homeCarsStyles as styles } from '../assets/dummyStyles'
-import carsdata from '../assets/'
+import React from "react";
+import { homeCarsStyles as styles } from "../assets/dummyStyles";
+import carsData from "../assets/carsData";
+import { Zap } from 'lucide-react';
 
 function HomeCars() {
   return (
-    <div>HomeCars</div>
-  )
+    <div className={styles.container}>
+      {/*Header*/}
+
+      <div className={styles.headerContainer}>
+        <div className={styles.premiumBadge}>
+            <Zap className="h-4 w-4 text-amber-400 mr-2"/>
+            <span className={styles.premiumText}>Premium Cars</span>
+        </div>
+        <h1 className={styles.}></h1>
+      </div>
+    </div>
+  );
 }
 
-export default HomeCars
+export default HomeCars;
