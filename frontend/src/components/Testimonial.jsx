094bb91975a57ef96518c2b5bd687f3d80e3bf58
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-12-03 23:02:39 +0530

    add badge text

--- frontend/src/components/Testimonial.jsx
+++ frontend/src/components/Testimonial.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import { testimonialStyles as styles  } from '../assets/dummyStyles'
-import testimonials from '../assets/TestimonialData'
+import React from "react";
+import { testimonialStyles as styles } from "../assets/dummyStyles";
+import testimonials from "../assets/TestimonialData";
 
 function Testimonial() {
   return (
     <div className={styles.container}>
-        <div className={styles.innerContainer}>
-            <div className={styles.headerContainer}>
-                <div className={styles.badge}>
-                    <FaCar className={`${styles.quoteIcon} mr-2`} />
-                
-                </div>
-            </div>
+      <div className={styles.innerContainer}>
+        <div className={styles.headerContainer}>
+          <div className={styles.badge}>
+            <FaCar className={`${styles.quoteIcon} mr-2`} />
+            <span className={styles.badgeText}>Customer Experiences</span>
+          </div>
+          <h1 className={styles.title}>
+            Premium <span className={styles.accentText}>Drive</span>Experiences
+          </h1>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Testimonial
+export default Testimonial;
