45dca0bbe62f7ac7f9d63356b2537bde8564a13e
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-12-05 00:57:26 +0530

    add docker files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,4 +7,17 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+
+   // Optional: Ensures correct build output
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+
+  // Required ONLY in dev mode, not used in Docker
+  server: {
+    port: 5173,
+    open: false,
+  }
+
 })
